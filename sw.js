be1177dd59f0d98ad101b6cd0448423dd901744b
nfmcglinchey <nfmcglinchey@icloud.com>
2025-02-21 20:52:06 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,28 @@
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open('game-v1').then((cache) => {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/game.js',
-                '/assets/player.png',
-                '/assets/enemy.png',
-                '/assets/platform.png',
-                '/assets/bike.png',
-                '/assets/key.png',
-                '/assets/door.png',
-                '/assets/boss.png'
-            ]);
-        })
-    );
+  event.waitUntil(
+    caches.open('game-v1').then((cache) => {
+      return cache.addAll([
+        'index.html',
+        'game.js',
+        'manifest.json',
+        'assets/player.png',
+        'assets/enemy.png',
+        'assets/platform.png',
+        'assets/bike.png',
+        'assets/key.png',
+        'assets/door.png',
+        'assets/boss.png',
+        'assets/icons/icon-192.png',
+        'assets/icons/icon-512.png'
+      ]);
+    })
+  );
 });
 
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });

85cad429f350bf985c57f3f471fb32f5212c5441
nfmcglinchey <nfmcglinchey@icloud.com>
2025-02-21 21:45:02 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,45 @@
-self.addEventListener('install', (event) => {
+const CACHE_NAME = 'game-v2';
+const urlsToCache = [
+  'index.html',
+  'game.js',
+  'manifest.json',
+  'assets/player.png',
+  'assets/enemy.png',
+  'assets/platform.png',
+  'assets/bike.png',
+  'assets/key.png',
+  'assets/door.png',
+  'assets/boss.png',
+  'assets/icons/icon-192.png',
+  'assets/icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('game-v1').then((cache) => {
-      return cache.addAll([
-        'index.html',
-        'game.js',
-        'manifest.json',
-        'assets/player.png',
-        'assets/enemy.png',
-        'assets/platform.png',
-        'assets/bike.png',
-        'assets/key.png',
-        'assets/door.png',
-        'assets/boss.png',
-        'assets/icons/icon-192.png',
-        'assets/icons/icon-512.png'
-      ]);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(name => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      );
     })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });

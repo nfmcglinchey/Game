677b6dc96e357bccb8f529df8f04884c4d02bb59
nfmcglinchey <nfmcglinchey@icloud.com>
2025-02-24 14:40:01 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,7 @@
+// Updated cache name to force refresh if needed
 const CACHE_NAME = 'game-v3';
+
+// Make sure these paths match the files you want to cache
 const urlsToCache = [
   'index.html',
   'game.js',
@@ -9,9 +12,9 @@ const urlsToCache = [
   'assets/bike.png',
   'assets/key.png',
   'assets/door.png',
-  'assets/boss.png',
-  'assets/icons/icon-192.png',
-  'assets/icons/icon-512.png'
+  'assets/doc.png', // doc.png is your "boss" sprite
+  'assets/icons/icon-72.png',
+  'assets/icons/icon-152.png'
 ];
 
 self.addEventListener('install', event => {
